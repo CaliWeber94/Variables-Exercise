996c534478363d9ec8f8333892dca36b390f73dd
Cali Weber <calista.weber1@gmail.com>
2023-07-20 11:25:00 -0700

    variables practice

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string myCatsName = "Skye";
+            int weight = 6;
+            char intial = 'S';
+                bool isCute = true;
+            double catsWeight = 8;
+                decimal priceOfCat = 100.00m;
+
+            Console.WriteLine($"Hello, my name is {myCatsName}, I am {weight} pounds but I used to be {catsWeight} pounds! My initial is {intial}. It is {isCute} that I am cute!  When my mom got me I cost {priceOfCat} dollars.");
+
         }
     }
 }
